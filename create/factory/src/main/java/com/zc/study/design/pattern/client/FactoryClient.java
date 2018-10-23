d5cf83855d3019d22209ec4fb5b0cf87efe604cc
Daniel <zhengcheng_email@163.com>
2018-10-23 22:34:26 +0800

    工厂完成，添加结构类型模块，添加适配器

---
+++ create/factory/src/main/java/com/zc/study/design/pattern/client/FactoryClient.java
@@ -0,0 +1,10 @@
+package com.zc.study.design.pattern.client;
+
+/**
+ * 工厂模式客户端
+ *
+ * @author Daniel Zheng
+ * @create 2018-10-23 下午10:02
+ **/
+public class FactoryClient {
+}
---
+++ create/factory/src/main/java/com/zc/study/design/pattern/create/factory/ProductBFactoryImpl.java
@@ -0,0 +1,10 @@
+package com.zc.study.design.pattern.create.factory;
+
+/**
+ * 产品B工厂
+ *
+ * @author Daniel Zheng
+ * @create 2018-10-23 下午10:06
+ **/
+public class ProductBFactoryImpl {
+}
